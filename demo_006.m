5c56732f1c75445c70bd2893eb6e12eb56f973c9
Heiko Schütt <heiko.schuett@uni-tuebingen.de>
2014-11-28 12:04:00 +0100

    added demos prior and results struct

---
+++ demo_006.m
@@ -0,0 +1,77 @@
+% demo_006 fields of the results struct - Which information is contained in
+% the result of Psignifit
+
+% to have some data we use the data from demo_001
+data =    [...
+    0.0010,   45.0000,   90.0000;...
+    0.0015,   50.0000,   90.0000;...
+    0.0020,   44.0000,   90.0000;...
+    0.0025,   44.0000,   90.0000;...
+    0.0030,   52.0000,   90.0000;...
+    0.0035,   53.0000,   90.0000;...
+    0.0040,   62.0000,   90.0000;...
+    0.0045,   64.0000,   90.0000;...
+    0.0050,   76.0000,   90.0000;...
+    0.0060,   79.0000,   90.0000;...
+    0.0070,   88.0000,   90.0000;...
+    0.0080,   90.0000,   90.0000;...
+    0.0100,   90.0000,   90.0000];
+
+
+% just initialize the options struct
+options=struct;
+
+% and run psignifit
+
+res=psignifit(data,options);
+% now we can have a look at the res struct and all its fields
+
+
+%% list of result struct fields
+% Here we list all fields of the result struct in the format
+% options.[field]        = short description
+%
+% after it follow some explanation and allowed values
+
+
+%% res.Fit = the fitted parameter of the psychometric function
+% Which kind of fit was performed is determined by the options you set. 
+% It might be mean, median or MAP.
+% The order of reported parameters is
+% [threshold,width,lambda,gamma,sigma]
+
+%% res.conf_Intervals = confidence intervals for the fit
+% the confidence intervals for the 5 parameters.  
+% The order of reported parameters is
+% [threshold,width,lambda,gamma,sigma]
+
+%% res.data = data used for the fit
+% the array used as data input for psignifit
+
+%% res.options = the options struct used for the fit
+% contains all options set for the fit including automatically set values
+
+%% res.timestamp = When the data result was created
+
+%% res.Posterior = posterior density at the gridpoints
+% normalized Posterior density evaluated at the final gridpoints
+
+%% res.weight = integration weight for each gridpoint
+% this is the volume of parameter space each gridpoint "speaks for". This
+% is needed for integrations over the space.
+
+%% res.X1D = positions of the gridpoints on the 5 dimensions
+% a cell array of vectors
+
+%% res.marginals   = marginal densities for the 5 parameters
+%% res.marginalsX  = positions of the marginal evaluations
+%% res.marginalsW  = integration weight for each gridpoint
+% Used together these three represent the marginal posterior distributions
+
+
+%% res.logPmax, res.integral = normalization constants
+% the maximal log-likelihood which is subtracted prior to computing the
+% exponential to avoid numerical problems 
+% and the integral over the likelihood accross the parameter space, used
+% for normalizing into a probability.
+
---
+++ plotPrior.m
@@ -0,0 +1,207 @@
+function plotPrior(res)
+% this function creates the plot illustrating the priors on the different
+% parameters
+%
+% this is meant to be a 3 column, 2 rows multipanel plot. 
+
+% parameters 
+lineW  = 2;
+lineC  = [0,105/255,170/255];
+markerSize = 30;
+
+data = res.data;
+
+
+% get correct stimulus range
+if numel(res.options.stimulusRange)<=1
+    res.options.stimulusRange = [min(data(:,1)),max(data(:,1))];
+    stimRangeSet = false;
+else
+    stimRangeSet = true;
+end
+range = res.options.stimulusRange(2)-res.options.stimulusRange(1);
+
+% get borders for width
+% minimum = minimal difference of two stimulus levels
+if length(unique(data(:,1)))>1 && ~stimRangeSet
+    widthmin  = min(diff(sort(unique(data(:,1)))));
+else
+    widthmin = 100*eps(res.options.stimulusRange(2));
+end
+% maximum = spread of the data
+widthmax  = range;
+
+
+
+% calculate mean for priors
+for itheta = 1:5
+    switch itheta
+        case 1
+            x = linspace(res.options.stimulusRange(1)-.5*range,res.options.stimulusRange(2)+.5*range,10000);
+        case 2
+            x = linspace(min(res.X1D{itheta}),max(res.X1D{2}),10000);
+        case 3
+            x = linspace(0,.5,10000);
+        case 4
+            x = linspace(0,.5,10000);
+        case 5
+            x = linspace(0,1,10000);
+    end
+    y = res.options.priors{itheta}(x);
+    theta(itheta) = sum(x.*y)./sum(y);
+end
+if strcmp(res.options.expType,'equalAsymptote')
+    theta(4) = theta(3);
+end
+if strcmp(res.options.expType,'nAFC')
+    theta(4) = 1./res.options.expN;
+end
+
+
+% get limits for the psychometric function plots
+
+xLimit = [res.options.stimulusRange(1)-.5*range,res.options.stimulusRange(2)+.5*range];
+
+% The first row shows the prior marginal densities
+
+%% threshold 
+xthresh = linspace(res.options.stimulusRange(1)-.5*range,res.options.stimulusRange(2)+.5*range,10000);
+ythresh = res.options.priors{1}(xthresh);
+wthresh = conv(diff(xthresh),.5*[1,1]);
+cthresh = cumsum(ythresh.*wthresh);
+subplot(2,3,1)
+plot(xthresh,ythresh,'LineWidth',lineW,'Color',lineC)
+hold on
+xlim(xLimit)
+title('Threshold','FontSize',18)
+ylabel('Density','FontSize',18)
+
+
+subplot(2,3,4)
+plot(data(:,1),0,'k','LineStyle','none','Marker','.','MarkerSize',markerSize*.75)
+hold on
+ylabel('Percent Correct','FontSize',18)
+xlim(xLimit)
+
+x = linspace(xLimit(1),xLimit(2),10000);
+for idot= 1:5
+    switch idot
+        case 1
+            xcurrent = theta(1);
+            color = 'k';
+        case 2
+            xcurrent = min(xthresh);
+            color = [1,200/255,0];
+        case 3
+            xcurrent = xthresh(find(cthresh>=.25,1,'first'));
+            color = 'r';
+        case 4
+            xcurrent = xthresh(find(cthresh>=.75,1,'first'));
+            color = 'b';
+        case 5 
+            xcurrent = max(xthresh);
+            color = 'g';
+    end
+    y = 100*(theta(4)+ (1-theta(3)-theta(4)).* res.options.sigmoidHandle(x,xcurrent,theta(2)));
+    subplot(2,3,4)
+    plot(x,y,'-','LineWidth',lineW,'Color',color)
+    subplot(2,3,1)
+    plot(xcurrent,res.options.priors{1}(xcurrent),'Color',color,'LineStyle','none','Marker','.','MarkerSize',markerSize)
+end
+
+
+%% width
+
+
+xwidth = linspace(widthmin,3.*widthmax,10000);
+ywidth = res.options.priors{2}(xwidth);
+wwidth = conv(diff(xwidth),.5*[1,1]);
+cwidth = cumsum(ywidth.*wwidth);
+subplot(2,3,2)
+plot(xwidth,ywidth,'LineWidth',lineW,'Color',lineC)
+hold on
+xlim([widthmin,3*widthmax])
+title('Width','FontSize',18)
+
+subplot(2,3,5)
+plot(data(:,1),0,'k','LineStyle','none','Marker','.','MarkerSize',markerSize*.75)
+hold on
+xlim(xLimit)
+xlabel('Stimulus Level','FontSize',18)
+
+x = linspace(xLimit(1),xLimit(2),10000);
+for idot= 1:5
+    switch idot
+        case 1
+            xcurrent = theta(2);
+            color = 'k';
+        case 2
+            xcurrent = min(xwidth);
+            color = [1,200/255,0];
+        case 3
+            xcurrent = xwidth(find(cwidth>=.25,1,'first'));
+            color = 'r';
+        case 4
+            xcurrent = xwidth(find(cwidth>=.75,1,'first'));
+            color = 'b';
+        case 5 
+            xcurrent = max(xwidth);
+            color = 'g';
+    end
+    y = 100*(theta(4)+ (1-theta(3)-theta(4)).* res.options.sigmoidHandle(x,theta(1),xcurrent));
+    subplot(2,3,5)
+    plot(x,y,'-','LineWidth',lineW,'Color',color)
+    subplot(2,3,2)
+    plot(xcurrent,res.options.priors{2}(xcurrent),'Color',color,'LineStyle','none','Marker','.','MarkerSize',markerSize)
+end
+
+%% lapse
+
+xlapse = linspace(0,.5,10000);
+ylapse = res.options.priors{3}(xlapse);
+wlapse = conv(diff(xlapse),.5*[1,1]);
+clapse = cumsum(ylapse.*wlapse);
+subplot(2,3,3)
+plot(xlapse,ylapse,'LineWidth',lineW,'Color',lineC)
+hold on
+xlim([0,.5])
+title('\lambda','FontSize',18)
+
+subplot(2,3,6)
+plot(data(:,1),0,'k','LineStyle','none','Marker','.','MarkerSize',markerSize*.75)
+hold on
+xlim(xLimit)
+
+
+x = linspace(xLimit(1),xLimit(2),10000);
+for idot= 1:5
+    switch idot
+        case 1
+            xcurrent = theta(3);
+            color = 'k';
+        case 2
+            xcurrent = 0;
+            color = [1,200/255,0];
+        case 3
+            xcurrent = xlapse(find(clapse>=.25,1,'first'));
+            color = 'r';
+        case 4
+            xcurrent = xlapse(find(clapse>=.75,1,'first'));
+            color = 'b';
+        case 5 
+            xcurrent = .5;
+            color = 'g';
+    end
+    y = 100*(theta(4)+ (1-xcurrent-theta(4)).* res.options.sigmoidHandle(x,theta(1),theta(2)));
+    subplot(2,3,6)
+    plot(x,y,'-','LineWidth',lineW,'Color',color)
+    subplot(2,3,3)
+    plot(xcurrent,res.options.priors{3}(xcurrent),'Color',color,'LineStyle','none','Marker','.','MarkerSize',markerSize)
+end
+
+set(gcf, 'Position',[200,300,1000,600])
+for i=1:6    
+    subplot(2,3,i)
+    box off;
+end
+    
