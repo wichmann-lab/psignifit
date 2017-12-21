0efc97702803a003b2d7d0d6e14ef9e95dcd46ab
Heiko Schütt <heiko.schuett@uni-tuebingen.de>
2017-12-21 14:57:02 +0100

    added bootstrapping to mimic psignifit 2 due to public demand

--- getDeviance.m
+++ getDeviance.m
@@ -1,4 +1,12 @@
-function [devianceResiduals,deviance] = getDeviance(result)
+function [devianceResiduals,deviance,samples_deviance,samples_devianceResiduals] = getDeviance(result,Nsamples)
+%function [devianceResiduals,deviance,samples_deviance,samples_devianceResiduals] = getDeviance(result,Nsamples)
+% This function calculates the observed deviance from the model. 
+%
+% If you ask for more than 2 outputs it also calculates bootstrap samples
+% from the function and calculates the deviance & deviance residuals for
+% these samples. This was implemented due to public demand to replicate a
+% funcionality of psignifit 2. There it corresponds to the sampling method
+% without refitting. 
 
 pPred = result.psiHandle(result.data(:,1));
 pMeasured = result.data(:,2)./result.data(:,3);
@@ -12,3 +20,20 @@ devianceResiduals = -2*sign(pMeasured-pPred).*(loglikelihoodMeasured - loglikeli
 
 deviance = sum(abs(devianceResiduals));
 
+if nargout > 2
+    if ~exist('Nsamples','var') || isempty(Nsamples)
+        Nsamples = 5000;
+    end
+    
+    samples_devianceResiduals = nan(Nsamples,size(result.data,1));
+    for iData = 1:size(result.data,1)
+        samp_dat = binornd(result.data(iData,3),pPred(iData),Nsamples,1);
+        pMeasured = samp_dat./result.data(iData,3);
+        loglikelihoodPred = samp_dat.*log(pPred(iData))+(result.data(iData,3)-samp_dat).*log((1-pPred(iData)));
+        loglikelihoodMeasured = samp_dat.*log(pMeasured)+(result.data(iData,3)-samp_dat).*log((1-pMeasured));
+        loglikelihoodMeasured(pMeasured==1) = 0;
+        loglikelihoodMeasured(pMeasured==0) = 0;
+        samples_devianceResiduals(:,iData) = -2*sign(pMeasured-pPred(iData)).*(loglikelihoodMeasured - loglikelihoodPred);
+    end
+    samples_deviance = sum(abs(samples_devianceResiduals),2);
+end
--- plotsModelfit.m
+++ plotsModelfit.m
@@ -5,6 +5,8 @@ function plotsModelfit(res)
 %
 % These are the same plots as presented in psignifit 2 for this purpose.
 %
+% The deviance bootstrap samples plot is generated with getDeviance, i.e.
+% does not contain refitting.
 
 stimRange = [min(res.data(:,1)),max(res.data(:,1))];
 stimRange = [1.1*stimRange(1)-.1*stimRange(2),1.1*stimRange(2)-.1*stimRange(1)];
@@ -17,7 +19,7 @@ set(gcf,'Position',Pos);
 subplot(1,3,1)
 % the psychometric function
 x = linspace(stimRange(1),stimRange(2),1000);
-y = res.Fit(4)+(1-res.Fit(3)-res.Fit(4))*res.options.sigmoidHandle(x,res.Fit(1),res.Fit(2));
+y = res.psiHandle(x);
 
 hold on
 plot(x,y,'k')
@@ -35,46 +37,60 @@ xlabel('Stimulus Level','FontSize',14)
 ylabel('Percent Correct','FontSize',14)
 title('Psychometric Function','FontSize',20)
 box off
+set(gca,'TickDir','out')
 
 subplot(1,3,2)
 % stimulus level vs. deviance
-pPred = res.psiHandle(res.data(:,1));
-pMeasured = res.data(:,2)./res.data(:,3);
-
-loglikelihoodPred = res.data(:,2).*log(pPred)+(res.data(:,3)-res.data(:,2)).*log((1-pPred));
-loglikelihoodMeasured = res.data(:,2).*log(pMeasured)+(res.data(:,3)-res.data(:,2)).*log((1-pMeasured));
-loglikelihoodMeasured(pMeasured==1) = 0;
-loglikelihoodMeasured(pMeasured==0) = 0;
-
-deviance= -2*sign(pMeasured-pPred).*(loglikelihoodMeasured - loglikelihoodPred);
+[devianceResiduals,deviance,deviance_samples] = getDeviance(res);
+% pPred = res.psiHandle(res.data(:,1));
+% pMeasured = res.data(:,2)./res.data(:,3);
+% 
+% loglikelihoodPred = res.data(:,2).*log(pPred)+(res.data(:,3)-res.data(:,2)).*log((1-pPred));
+% loglikelihoodMeasured = res.data(:,2).*log(pMeasured)+(res.data(:,3)-res.data(:,2)).*log((1-pMeasured));
+% loglikelihoodMeasured(pMeasured==1) = 0;
+% loglikelihoodMeasured(pMeasured==0) = 0;
+% 
+% deviance= -2*sign(pMeasured-pPred).*(loglikelihoodMeasured - loglikelihoodPred);
 xValues = linspace(min(res.data(:,1)),max(res.data(:,1)),1000);
 
-plot(res.data(:,1),deviance,'k.','MarkerSize',20)
+plot(res.data(:,1),devianceResiduals,'k.','MarkerSize',20)
 xlabel('Stimulus Level','FontSize',14)
-ylabel('Deviance','FontSize',14)
+ylabel('Deviance Residuals','FontSize',14)
 hold on
-linefit = polyfit(res.data(:,1),deviance,1);
+linefit = polyfit(res.data(:,1),devianceResiduals,1);
 plot(xValues,polyval(linefit,xValues),'k-')
-linefit = polyfit(res.data(:,1),deviance,2);
+linefit = polyfit(res.data(:,1),devianceResiduals,2);
 plot(xValues,polyval(linefit,xValues),'k--')
-linefit = polyfit(res.data(:,1),deviance,3);
+linefit = polyfit(res.data(:,1),devianceResiduals,3);
 plot(xValues,polyval(linefit,xValues),'k:')
 title('Shape Check','FontSize',20)
 box off
+set(gca,'TickDir','out')
 
 subplot(1,3,3)
 
-BlockN  = (1:length(deviance))';
+BlockN  = (1:length(devianceResiduals))';
 xValues = linspace(min(BlockN),max(BlockN),1000);
-plot(BlockN,deviance,'k.','MarkerSize',20)
+plot(BlockN,devianceResiduals,'k.','MarkerSize',20)
 hold on
-linefit = polyfit(BlockN,deviance,1);
+linefit = polyfit(BlockN,devianceResiduals,1);
 plot(xValues,polyval(linefit,xValues),'k-')
-linefit = polyfit(BlockN,deviance,2);
+linefit = polyfit(BlockN,devianceResiduals,2);
 plot(xValues,polyval(linefit,xValues),'k--')
-linefit = polyfit(BlockN,deviance,3);
+linefit = polyfit(BlockN,devianceResiduals,3);
 plot(xValues,polyval(linefit,xValues),'k:')
 xlabel('Block #','FontSize',14)
-ylabel('Deviance','FontSize',14)
+ylabel('Deviance Residuals','FontSize',14)
 title('Time Dependence?','FontSize',20)
 box off
+set(gca,'TickDir','out')
+
+figure 
+[N,x] = hist(deviance_samples,100);
+bar(x,N,'FaceColor',[.5,.5,.5],'EdgeColor',[0.5,0.5,0.5])
+hold on
+plot([deviance,deviance] ,[0,max(get(gca,'ylim'))],'k-','linewidth',2)
+box off 
+set(gca,'TickDir','out')
+xlabel('Deviance','FontSize',14)
+ylabel('# Bootstrap Samples','FontSize',14)
